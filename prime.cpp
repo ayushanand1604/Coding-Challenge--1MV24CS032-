ef532b0e1ecd0e857b042aa81aa58fb3cd97cb4c
Ayush Anand <ayushanand@192.168.0.100>
2025-04-24 22:27:03 +0530

    added new

--- prime.cpp
+++ prime.cpp
@@ -5,7 +5,9 @@ int main() {
 	int n;
 	cin>>n;
 	int count=0;
-	for(int i=2;i<=n;i++){
+    if(n==1)
+	cout<<n<<" is neither prime nor composite"<<endl;
+	else{for(int i=2;i<=n;i++){
 	    for(int j=1;j<=i;j++){
 	    if(i%j==0)
 	        count++;
@@ -17,5 +19,6 @@ int main() {
 	        cout<<i<<" is a prime number"<<endl;
 	    count=0;
 	    }
+    }
 	return 0;
 }
